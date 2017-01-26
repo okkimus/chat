49399c31a18ff8988f51cb35ebd23439d9575ad3
Mikko Metsäranta <misame@utu.fi>
2017-01-27 00:11:08 +0200

    add navbar and updated views

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -10,6 +10,7 @@ class RoomsController < ApplicationController
   # GET /rooms/1
   # GET /rooms/1.json
   def show
+    @viesti = Message.new
   end
 
   # GET /rooms/new
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :rooms
   resources :users
   resources :messages
+
+  root 'rooms#index'
+  #resources :messages
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
